19bd144b685595a6a8abe8733d0cc62b36fec198
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-22 11:02:08 -0500

    tested budget.sqlite is working

--- server.js
+++ server.js
@@ -4,4 +4,25 @@ const server = express();
 
 // your code here
 
+
+
+
+const dbaccount = require('./data/accounts-model.js');
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>DB TEST</h2>
+  `)
+})
+
+server.get('/api/accounts', (req, res) =>{
+  dbaccount.find()
+    .then( project => {
+      res.status(200).json(project)
+    })
+    .catch( err => {
+      res.status(500).json({ error: err, message:"Could not update data"})
+    })
+})
+
 module.exports = server;
