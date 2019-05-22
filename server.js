3e7c16123bfba7cc3f9b6797d221b90587f08dc9
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-22 13:06:25 -0500

    Tested SQL & Works hit MVP

--- server.js
+++ server.js
@@ -5,24 +5,78 @@ const server = express();
 // your code here
 
 
-
-
 const dbaccount = require('./data/accounts-model.js');
 
-server.get('/', (req, res) => {
-  res.send(`
-    <h2>DB TEST</h2>
-  `)
-})
+  server.get('/', (req, res) => {
+    res.send(`
+      <h2>DB TEST</h2>
+    `)
+  })
 
-server.get('/api/accounts', (req, res) =>{
-  dbaccount.find()
-    .then( project => {
-      res.status(200).json(project)
+  server.get('/api/accounts', (req, res) =>{
+    dbaccount.find()
+      .then( accounts => {
+        res.status(200).json(accounts)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+  server.get("/api/accounts/:id", (req, res) =>{
+    const id = req.params.id
+    dbaccount.findById(id)
+      .then( projectaccounts => {
+        res.status(200).json(projectaccounts)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+  server.post('/api/accounts', (req, res) => {
+    const account = req.body
+    dbaccount.add(account)
+      .then(account => {
+        res.status(200).json(account)
+      })
+      .catch(err => {
+        res.status(500).json({error: err, message: "Could not update data"})
+      })
     })
-    .catch( err => {
-      res.status(500).json({ error: err, message:"Could not update data"})
-    })
-})
+
+  server.put('/api/accounts/:id', (req, res) => {
+    const updateAccounts = req.body
+    const id = req.params.id
+
+    dbaccount.update(id, updateAccounts)
+      .then( project => {
+        res.status(200).json(project)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+  server.delete('/api/accounts/:id', (req, res)=>{
+    const accountsid = req.params.id
+    dbaccount.remove(accountsid)
+      .then( account =>{
+        if(account){
+          dbaccount.remove(account).then(
+            removeaccount => {
+              res.status(201).json(removeaccount)
+            }
+          )
+        }else{
+          res.status(404).json({ error: err, mesage : "User does not exist"})
+        }
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+
 
 module.exports = server;
